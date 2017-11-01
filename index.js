00b4fd253bd4ec788f9d3b7154c39405d8802df0
Donte Hightower <dontehightower12@gmail.com>
2017-11-01 14:00:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function add2(n) {
   return n + two
 
   // Feel free to move things around!
+  //const two = 2 <-- used to be here
 }
 
 var funkyFunction = function() {
@@ -27,4 +28,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()
+
